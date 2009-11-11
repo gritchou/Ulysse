ff4eb8480f5ded5feab05e37b8ba233908205261
Cynthia FLORENTIN <cynthia.florentin54@gmail.com>
2009-11-11 18:14:06 +0100

    [#8920] Creation de l'interface SearchResult

--- factory/factory-core/src/main/java/org/qualipso/factory/indexing/SearchResult.java
+++ factory/factory-core/src/main/java/org/qualipso/factory/indexing/SearchResult.java
@@ -16,8 +16,6 @@
  */
 package org.qualipso.factory.indexing;
 
-import java.io.Serializable;
-
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
@@ -26,84 +24,125 @@ import org.qualipso.factory.FactoryResourceIdentifier;
 
 
 /**
- * @author Jerome Blanchard (jayblanc@gmail.com)
- * @date 20 May 2009
+ * Class which represent the result of a search with a query
+ * @author Benjamin DREUX
+ * @author cynthia FLORENTIN 
  */
 //TODO move this class to the factory level (FactorySearchResult)
 @XmlType(name = "SearchResult", namespace = "http://org.qualipso.factory.ws/index", propOrder =  {
     "path", "score", "explain", "name", "type", "identifier"}
 )
-@SuppressWarnings("serial")
-public class SearchResult implements Serializable {
-    private String path;
+public class SearchResult implements SearchResultI {
+    
+	private static final long serialVersionUID = 1L;
+	private String path;
     private float score;
     private String explain;
     private String name;
     private String type;
     private FactoryResourceIdentifier resourceIdentifier;
 
-    public SearchResult() {
-    }
-
+    /**
+     * @see SearchResultI#getPath()
+     */
     @XmlAttribute(name = "id", required = true)
     public String getPath() {
         return path;
     }
-
+    
+    /**
+     * @see SearchResultI#setPath(String)
+     */
     public void setPath(String path) {
         this.path = path;
     }
-
+    
+    /**
+     * @see SearchResultI#getScore()
+     */
     @XmlAttribute(name = "score", required = true)
     public float getScore() {
         return score;
     }
 
+    /**
+     * @see SearchResultI#setScore(float)
+     */
     public void setScore(float score) {
         this.score = score;
     }
 
+    /**
+     * @see SearchResultI#getExplain()
+     */
     @XmlAttribute(name = "explain", required = true)
     public String getExplain() {
         return explain;
     }
-
+    
+    /**
+     * @see SearchResultI#setExplain(String)
+     */
     public void setExplain(String explain) {
         this.explain = explain;
     }
-
+    
+    /**
+     * @see SearchResultI#getName()
+     */
     @XmlAttribute(name = "name", required = true)
     public String getName() {
         return name;
     }
-
+    
+    /**
+     * @see SearchResultI#setName(String)
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    /**
+     * @see SearchResultI#getType()
+     */
     @XmlAttribute(name = "type", required = true)
     public String getType() {
         return type;
     }
-
+    
+    /**
+     * @see SearchResultI#setType(String)
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * @see SearchResultI#getResourceIdentifier()
+     */
     @XmlTransient
     public FactoryResourceIdentifier getResourceIdentifier() {
         return resourceIdentifier;
     }
-
+    
+    /**
+     * @see SearchResultI#setResourceIdentifier(FactoryResourceIdentifier)
+     */
     public void setResourceIdentifier(FactoryResourceIdentifier resourceIdentifier) {
         this.resourceIdentifier = resourceIdentifier;
     }
     
+    /**
+     * @see SearchResultI#getIdentifier()
+     */
     @XmlAttribute(name = "factory-resource-identifier", required = true)
     public String getIdentifier() {
         return resourceIdentifier.serialize();
     }
 
+    /**
+     * @see SearchResultI#setIdentifier(String)
+     */
     public void setIdentifier(String identifier) {
         this.resourceIdentifier = FactoryResourceIdentifier.deserialize(identifier);
     }
