1bb74425e7ecb31f5fea950bd6612f14904e5607
Nicolas HENRY <icewil@gmail.com>
2009-11-14 21:04:41 +0100

    Revert "idexation fichier"
    This reverts commit 3914c6b743fc0dad05efb17629c5e462e792bab9.

--- factory/factory-core/src/main/java/org/qualipso/factory/core/CoreServiceBean.java
+++ factory/factory-core/src/main/java/org/qualipso/factory/core/CoreServiceBean.java
@@ -39,7 +39,7 @@ import org.qualipso.factory.core.entity.FileDataSource;
 import org.qualipso.factory.core.entity.Folder;
 import org.qualipso.factory.core.entity.Link;
 import org.qualipso.factory.membership.MembershipService;
-import org.qualipso.factory.notification.Event;
+import org.qualipso.factory.eventqueue.entity.Event;
 import org.qualipso.factory.notification.NotificationService;
 import org.qualipso.factory.security.pap.PAPService;
 import org.qualipso.factory.security.pap.PAPServiceHelper;
@@ -51,37 +51,35 @@ import eu.medsea.mimeutil.MimeUtil;
  * @author Jerome Blanchard (jayblanc@gmail.com)
  * @date 24 july 2009
  */
-@Stateless(name = "Core", mappedName = FactoryNamingConvention.JNDI_SERVICE_PREFIX + "CoreService")
-@WebService(endpointInterface = "org.qualipso.factory.core.CoreService", targetNamespace = "http://org.qualipso.factory.ws/service/core", serviceName = "CoreService", portName = "CoreServicePort")
-@WebContext(contextRoot = "/factory-core", urlPattern = "/core")
+@Stateless(name = CoreService.SERVICE_NAME, mappedName = FactoryNamingConvention.SERVICE_PREFIX + CoreService.SERVICE_NAME)
+@WebService(endpointInterface = "org.qualipso.factory.core.CoreService", targetNamespace = FactoryNamingConvention.SERVICE_NAMESPACE + CoreService.SERVICE_NAME, serviceName = CoreService.SERVICE_NAME)
+@WebContext(contextRoot = FactoryNamingConvention.WEB_SERVICE_CORE_MODULE_CONTEXT, urlPattern = FactoryNamingConvention.WEB_SERVICE_URL_PATTERN_PREFIX + CoreService.SERVICE_NAME)
 @SOAPBinding(style = Style.RPC)
 @SecurityDomain(value = "JBossWSDigest")
 @EndpointConfig(configName = "Standard WSSecurity Endpoint")
 public class CoreServiceBean implements CoreService {
 	
-	private static final String SERVICE_NAME = "CoreService";
-	private static final String[] RESOURCE_TYPE_LIST = new String[] {"Folder", "File", "Link"};
 	private static Log logger = LogFactory.getLog(CoreServiceBean.class);
 	
 	private BindingService binding;
 	private PEPService pep;
-	private IndexationService indexation;
 	private PAPService pap;
 	private NotificationService notification;
 	private MembershipService membership;
 	private SessionContext ctx;
-//	private EntityManager em;
+	private EntityManager em;
+	
 	public CoreServiceBean() {
 	}
 	
-/*	@PersistenceContext
+	@PersistenceContext
 	public void setEntityManager(EntityManager em) {
 		this.em = em;
 	}
 
 	public EntityManager getEntityManager() {
 		return this.em;
-	}*/
+	}
 
 	@Resource
 	public void setSessionContext(SessionContext ctx) {
@@ -108,14 +106,6 @@ public class CoreServiceBean implements CoreService {
 
 	public PEPService getPEPService() {
 		return this.pep;
-	}
-	@EJB
-	public void setIndexationService(IndexationService indexation) {
-		this.indexation=indexation;
-	}
-
-	public IndexationService getIndexationService() {
-		return this.indexation;
 	}
 
 	@EJB
@@ -159,7 +149,7 @@ public class CoreServiceBean implements CoreService {
 			Link link = new Link();
 			link.setId(UUID.randomUUID().toString());
 			link.setLink(toPath);
-			//em.persist(link);
+			em.persist(link);
 			
 			binding.bind(link.getFactoryResourceIdentifier(), path);
 			
@@ -173,8 +163,7 @@ public class CoreServiceBean implements CoreService {
 			binding.setProperty(path, FactoryResourceProperty.OWNER, caller);
 			binding.setProperty(path, FactoryResourceProperty.POLICY_ID, policyId);
 			
-			indexation.index(link);
-			notification.throwEvent(new Event(path, caller, "Link", "core.link.create", ""));
+			notification.throwEvent(new Event(path, caller, Link.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Link.RESOURCE_NAME, "create"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to create the link at path " + path, e);
@@ -195,17 +184,17 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Link");
+			checkResourceType(identifier, Link.RESOURCE_NAME);
 		
-		/*	Link link = em.find(Link.class, identifier.getId());
+			Link link = em.find(Link.class, identifier.getId());
 			if ( link == null ) {
 				throw new CoreServiceException("unable to find a link for id " + identifier.getId());
 			}
 			link.setResourcePath(path);
 			
-			notification.throwEvent(new Event(path, caller, "Link", "core.link.read", ""));
+			notification.throwEvent(new Event(path, caller, Link.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Link.RESOURCE_NAME, "read"), ""));
 			
-			return link;*/
+			return link;
 		} catch ( Exception e ) {
 			logger.error("unable to read the link at path " + path, e);
 			throw new CoreServiceException("unable to read the link at path " + path, e);
@@ -224,18 +213,18 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Link");
+			checkResourceType(identifier, Link.RESOURCE_NAME);
 		
-		/*	Link link = em.find(Link.class, identifier.getId());
+			Link link = em.find(Link.class, identifier.getId());
 			if ( link == null ) {
 				throw new CoreServiceException("unable to find a link for id " + identifier.getId());
 			}
 			link.setLink(toPath);
-			em.merge(link);*/
+			em.merge(link);
 			
 			binding.setProperty(path, FactoryResourceProperty.LAST_UPDATE_TIMESTAMP, System.currentTimeMillis() + "");
 			
-			notification.throwEvent(new Event(path, caller, "Link", "core.link.update", ""));
+			notification.throwEvent(new Event(path, caller, Link.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Link.RESOURCE_NAME, "update"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to update the link at path " + path, e);
@@ -255,19 +244,17 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Link");
+			checkResourceType(identifier, Link.RESOURCE_NAME);
 		
-	/*		Link link = em.find(Link.class, identifier.getId());*/
+			Link link = em.find(Link.class, identifier.getId());
 			if ( link == null ) {
 				throw new CoreServiceException("unable to find a link for id " + identifier.getId());
 			}
-			//em.remove(link);
-			indexation.remove(link);
+			em.remove(link);
+			
 			binding.unbind(path);
-		
 			
-			
-			notification.throwEvent(new Event(path, caller, "Link", "core.link.delete", ""));
+			notification.throwEvent(new Event(path, caller, Link.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Link.RESOURCE_NAME, "delete"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to delete the link at path " + path, e);
@@ -289,7 +276,7 @@ public class CoreServiceBean implements CoreService {
 			folder.setId(UUID.randomUUID().toString());
 			folder.setName(name);
 			folder.setDescription(description);
-		//	em.persist(folder);
+			em.persist(folder);
 			
 			binding.bind(folder.getFactoryResourceIdentifier(), path);
 			
@@ -303,7 +290,7 @@ public class CoreServiceBean implements CoreService {
 			binding.setProperty(path, FactoryResourceProperty.OWNER, caller);
 			binding.setProperty(path, FactoryResourceProperty.POLICY_ID, policyId);
 			
-			notification.throwEvent(new Event(path, caller, "Folder", "core.folder.create", ""));
+			notification.throwEvent(new Event(path, caller, Folder.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Folder.RESOURCE_NAME, "create"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to create the folder at path " + path, e);
@@ -323,17 +310,17 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Folder");
+			checkResourceType(identifier, Folder.RESOURCE_NAME);
 		
-		/*	Folder folder = em.find(Folder.class, identifier.getId());
+			Folder folder = em.find(Folder.class, identifier.getId());
 			if ( folder == null ) {
 				throw new CoreServiceException("unable to find a folder for id " + identifier.getId());
 			}
 			folder.setResourcePath(path);
 			
-			notification.throwEvent(new Event(path, caller, "Folder", "core.folder.read", ""));
+			notification.throwEvent(new Event(path, caller, Folder.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Folder.RESOURCE_NAME, "read"), ""));
 			
-			return folder;*/
+			return folder;
 		} catch ( Exception e ) {
 			logger.error("unable to read the folder at path " + path, e);
 			throw new CoreServiceException("unable to read the folder at path " + path, e);
@@ -352,19 +339,19 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Folder");
+			checkResourceType(identifier, Folder.RESOURCE_NAME);
 		
-		/*	Folder folder = em.find(Folder.class, identifier.getId());
+			Folder folder = em.find(Folder.class, identifier.getId());
 			if ( folder == null ) {
 				throw new CoreServiceException("unable to find a folder for id " + identifier.getId());
 			}
 			folder.setName(name);
 			folder.setDescription(description);
-			em.merge(folder);*/
+			em.merge(folder);
 			
 			binding.setProperty(path, FactoryResourceProperty.LAST_UPDATE_TIMESTAMP, System.currentTimeMillis() + "");
 			
-			notification.throwEvent(new Event(path, caller, "Folder", "core.folder.update", ""));
+			notification.throwEvent(new Event(path, caller, Folder.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Folder.RESOURCE_NAME, "update"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to update the resource-folder at path " + path, e);
@@ -384,17 +371,17 @@ public class CoreServiceBean implements CoreService {
 			
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "Folder");
+			checkResourceType(identifier, Folder.RESOURCE_NAME);
 		
-		/*	Folder folder = em.find(Folder.class, identifier.getId());
+			Folder folder = em.find(Folder.class, identifier.getId());
 			if ( folder == null ) {
 				throw new CoreServiceException("unable to find a folder for id " + identifier.getId());
 			}
-			em.remove(folder);*/
+			em.remove(folder);
 			
 			binding.unbind(path);
 			
-			notification.throwEvent(new Event(path, caller, "Folder", "core.folder.delete", ""));
+			notification.throwEvent(new Event(path, caller, Folder.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, Folder.RESOURCE_NAME, "delete"), ""));
 		} catch ( Exception e ) {
 			ctx.setRollbackOnly();
 			logger.error("unable to delete the folder at path " + path, e);
@@ -457,7 +444,7 @@ public class CoreServiceBean implements CoreService {
 			file.setNbReads(0);
 			file.setBlob(Hibernate.createBlob(data));
 			file.setSize(file.getBlob().length());
-		//	em.persist(file);
+			em.persist(file);
 
 			binding.bind(file.getFactoryResourceIdentifier(), path);
 
@@ -471,7 +458,7 @@ public class CoreServiceBean implements CoreService {
 			binding.setProperty(path, FactoryResourceProperty.OWNER, caller);
 			binding.setProperty(path, FactoryResourceProperty.POLICY_ID, policyId);
 
-			notification.throwEvent(new Event(path, caller, "File", "core.file.create", ""));
+			notification.throwEvent(new Event(path, caller, File.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, File.RESOURCE_NAME, "create"), ""));
 		} catch (Exception e) {
 			ctx.setRollbackOnly();
 			logger.error("unable to create the file at path " + path, e);
@@ -492,17 +479,17 @@ public class CoreServiceBean implements CoreService {
 
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 
-			checkResourceType(identifier, "File");
+			checkResourceType(identifier, File.RESOURCE_NAME);
 
-	/*		File file = em.find(File.class, identifier.getId());
+			File file = em.find(File.class, identifier.getId());
 			if (file == null) {
 				throw new CoreServiceException("unable to find a file for id " + identifier.getId());
 			}
 			file.setResourcePath(path);
 
-			notification.throwEvent(new Event(path, caller, "File", "core.file.read", ""));
+			notification.throwEvent(new Event(path, caller, File.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, File.RESOURCE_NAME, "read"), ""));
 
-			return file;*/
+			return file;
 		} catch (Exception e) {
 			logger.error("unable to read the file at path " + path, e);
 			throw new CoreServiceException("unable to read the file at path " + path, e);
@@ -543,9 +530,9 @@ public class CoreServiceBean implements CoreService {
 
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			checkResourceType(identifier, "File");
+			checkResourceType(identifier, File.RESOURCE_NAME);
 
-			/*File file = em.find(File.class, identifier.getId());
+			File file = em.find(File.class, identifier.getId());
 			if (file == null) {
 				throw new CoreServiceException("unable to find a file for id " + identifier.getId());
 			}
@@ -561,11 +548,11 @@ public class CoreServiceBean implements CoreService {
 				file.setNbReads(0);
 				file.setSize(file.getBlob().length());
 			}
-			em.merge(file);*/
+			em.merge(file);
 
 			binding.setProperty(path, FactoryResourceProperty.LAST_UPDATE_TIMESTAMP, "" + System.currentTimeMillis());
 			
-			notification.throwEvent(new Event(path, caller, "File", "core.file.update", ""));
+			notification.throwEvent(new Event(path, caller, File.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, File.RESOURCE_NAME, "update"), ""));
 		} catch (Exception e) {
 			ctx.setRollbackOnly();
 			logger.error("unable to update the file at path " + path, e);
@@ -585,17 +572,17 @@ public class CoreServiceBean implements CoreService {
 
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 
-			checkResourceType(identifier, "File");
+			checkResourceType(identifier, File.RESOURCE_NAME);
 
-		/*	File file = em.find(File.class, identifier.getId());
+			File file = em.find(File.class, identifier.getId());
 			if (file == null) {
 				throw new CoreServiceException("unable to find a file for id " + identifier.getId());
 			}
-			em.remove(file);*/
+			em.remove(file);
 
 			binding.unbind(path);
 
-			notification.throwEvent(new Event(path, caller, "File", "core.file.delete", ""));
+			notification.throwEvent(new Event(path, caller, File.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, File.RESOURCE_NAME, "delete"), ""));
 		} catch (Exception e) {
 			ctx.setRollbackOnly();
 			logger.error("unable to delete the file at path " + path, e);
@@ -616,18 +603,18 @@ public class CoreServiceBean implements CoreService {
 
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 
-			checkResourceType(identifier, "File");
+			checkResourceType(identifier, File.RESOURCE_NAME);
 
-		/*	File file = em.find(File.class, identifier.getId());
+			File file = em.find(File.class, identifier.getId());
 			if (file == null) {
 				throw new CoreServiceException("unable to find a file for id " + identifier.getId());
 			}
 			file.setNbReads(file.getNbReads()+1);
-			em.merge(file);*/
+			em.merge(file);
 
 			FileData data = new FileData(new DataHandler(new FileDataSource(file)));
 
-			notification.throwEvent(new Event(path, caller, "File", "core.file.read-data", ""));
+			notification.throwEvent(new Event(path, caller, File.RESOURCE_NAME, Event.buildEventType(CoreService.SERVICE_NAME, File.RESOURCE_NAME, "read-data"), ""));
 
 			return data;
 		} catch (Exception e) {
@@ -664,19 +651,19 @@ public class CoreServiceBean implements CoreService {
 		try {
 			FactoryResourceIdentifier identifier = binding.lookup(path);
 			
-			if ( !identifier.getService().equals("CoreService") ) {
+			if ( !identifier.getService().equals(CoreService.SERVICE_NAME) ) {
 				throw new CoreServiceException("Resource " + identifier + " is not managed by Core Service");
 			}
 			
-			if ( identifier.getType().equals("Folder") ) {
+			if ( identifier.getType().equals(Folder.RESOURCE_NAME) ) {
 				return readFolder(path);
 			} 
 			
-			if ( identifier.getType().equals("File") ) {
+			if ( identifier.getType().equals(File.RESOURCE_NAME) ) {
 				return readFile(path);
 			}
 			
-			if ( identifier.getType().equals("Link") ) {
+			if ( identifier.getType().equals(Link.RESOURCE_NAME) ) {
 				return readLink(path);
 			}
 			
