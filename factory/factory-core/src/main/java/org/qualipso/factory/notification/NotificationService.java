df4f1470d4eeceb5256f4a12a09478842c13b8db
Jean-François Grand <gritchou@gmail.com>
2009-11-19 15:42:02 +0100

    Adding the javadoc the Notification Service Interface

--- factory/factory-core/src/main/java/org/qualipso/factory/notification/NotificationService.java
+++ factory/factory-core/src/main/java/org/qualipso/factory/notification/NotificationService.java
@@ -32,6 +32,36 @@ import org.qualipso.factory.eventqueue.entity.Event;
 import org.qualipso.factory.notification.entity.Rule;
 import org.qualipso.factory.FactoryNamingConvention;
 
+
+/**
+ * 
+ * NotificationService is the interface class for the Notification Core Component.
+ * There are four methods you can call from the Notification Service.
+ * This service allow you to match a rule to a specific queue.
+ * You can:
+ * <ul>
+ * <li>Register a rule to a queue.</li>
+ * <li>Unregister rule from a queue.</li>
+ * <li>Throw an Event.</li>
+ * <li>Get the list of the rules registered.</li>
+ * </ul>
+ * A rule is defined by a tuple:
+ * <ul>
+ * <li>subject</li>
+ * <li>object</li>
+ * <li>target</li>
+ * </ul>
+ * <p>
+ * A tuple (subject, object, target) example is : <code>("jeff", "commit", "/p1/t1/toto")</code>.
+ * Once this tuple is registered in the Notification Service, if jeff commits in <code>/p1/t1/toto</code>, 
+ * the rule will match this event and it will be pushed in the registered queue, but if he commits in <code>/p1/t1</code>, it will not match.
+ * <p>
+ * You can match more generic events : <code>("j*","com*","/p1/t1/*")</code>.
+ * With this rule, if jeff commits in <code>/p1/t1</code>, it will match, but if jack or john also commits in <code>/p1/t1</code>, it will match as well.
+ * 
+ * @author Jean-Francois Grand
+ * @version 11/19/2009
+ */
 @Remote
 @WebService(name = NotificationService.SERVICE_NAME, targetNamespace = FactoryNamingConvention.SERVICE_NAMESPACE + NotificationService.SERVICE_NAME)
 @SOAPBinding(style = SOAPBinding.Style.RPC)
@@ -40,15 +70,50 @@ public interface NotificationService extends FactoryService {
     public static final String SERVICE_NAME = "notification";
     public static final String[] RESOURCE_TYPE_LIST = new String[] {};
 
+    /**
+     * 
+     * Allows a user to register a rule to an event queue. A queue can be created using the eventqueue service.
+     * Rules are tuples: (subject, object, target).
+     * 
+     * @param subjectre the subject is the author of the action you want to register.
+     * @param objectre the object of the type of the action (commit, ...).
+     * @param targetre the target of the action (for example: the path to the project in which you commit, <code>/p1/t1</code>).
+     * @param queuePath the path to the queue in which events matching the rules will be pushed.
+     * @throws NotificationServiceException
+     */
     @WebMethod
     public void register(String subjectre, String objectre, String targetre, String queuePath) throws NotificationServiceException;
 
+    /**
+     * 
+     * Allows a user to unregister a rule from a queue.
+     * 
+     * @param subjectre the subject is the author of the action you want to unregister.
+     * @param objectre objectre the object of the type of the action (commit, ...).
+     * @param targetre the target of the action (for example: the path to the project in which you commit, <code>/p1/t1</code>).
+     * @param queuePath the path to the queue in which events matching the rules will be pushed.
+     * @throws NotificationServiceException
+     */
     @WebMethod
     public void unregister(String subjectre, String objectre, String targetre, String queuePath) throws NotificationServiceException;
     
+    /**
+     * 
+     * Allows to throw events from the Notification Service.
+     * 
+     * @param e the event to be thrown, see org.qualipso.factory.eventqueue.entity.Event.
+     * @throws NotificationServiceException
+     */
     @WebMethod
     public void throwEvent(Event e) throws NotificationServiceException;
     
+    /**
+     * 
+     * Lists the registered rules.
+     * 
+     * @return a list of the rules already registered in the Notification Service
+     * @throws NotificationServiceException
+     */
     @WebMethod
     public Rule[] list() throws NotificationServiceException;
 
