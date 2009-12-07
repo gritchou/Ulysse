fe28e94c555965402abba90ed26ed36e22f69b2f
Cynthia FLORENTIN <cynthia.florentin54@gmail.com>
2009-12-07 09:58:06 +0100

    Update Index && IndexingMessageBean && Add a new class IndexableService

---
+++ factory/factory-core/src/main/java/org/qualipso/factory/indexing/IndexableService.java
@@ -0,0 +1,37 @@
+package org.qualipso.factory.indexing;
+/*
+ *
+ * Qualipso Factory
+ * Copyright (C) 2006-2010 INRIA
+ * http://www.inria.fr - molli@loria.fr
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of LGPL. See licenses details in LGPL.txt
+ *
+ * Initial authors :
+ *
+ * Jérôme Blanchard / INRIA
+ * Pascal Molli / Nancy Université
+ * Gérald Oster / Nancy Université
+ *
+ */
+import org.qualipso.factory.FactoryService;
+/**
+  * @date 2 dec 2009
+  * @author Benjamin Dreux(benjiiiiii@gmail.com)
+  */
+
+
+/**
+  * Define the expectation to allow a service to index it's resource.
+  */
+public interface IndexableService extends FactoryService{
+
+    /**
+    * Get the indexable Content related to the resource located at a given path.
+    * @param path the path to the resource.
+    * @return the IndexableDocument correspoding to the resource to index.
+    * @thows IndexingServiceExcpetion if the resource can't be found.
+    */
+    public IndexableDocument getIndexableDocument(String path) throws IndexingServiceException;
+}
